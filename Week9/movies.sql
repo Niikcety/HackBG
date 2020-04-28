51a7161e5dcf16bb15cd76860843aeb47e81b46f
Niikcety <nikolaineshev1999@gmail.com>
2020-04-28 15:13:07 +0300

    Added solutions to the second part of the second task

--- Week9/movies.sql
+++ Week9/movies.sql
@@ -23,3 +23,44 @@ DELETE FROM STUDIO
 UPDATE MOVIE
   SET studioname = "FOX"
   WHERE title LIKE "%star%";
+
+SELECT starname
+  FROM STARSIN
+  JOIN MOVIESTAR ON STARSIN.starname = MOVIESTAR.name
+  WHERE movietitle = "Terms of Endearment" and gender = "M"
+
+SELECT starname
+  FROM STARSIN
+  JOIN MOVIE ON STARSIN.movietitle = MOVIE.TITLE
+  WHERE studioname = "MGM" and year = 1995
+
+ALTER TABLE STUDIO
+  ADD president_name TEXT
+
+UPDATE STUDIO
+  SET president_name = "Bob Chapek"
+  WHERE name = "Disney"
+
+UPDATE STUDIO
+  SET president_name = "Gary Ehrlich"
+  WHERE name = "Fox"
+
+UPDATE STUDIO
+  SET president_name = "Bill Hornbuckle"
+  WHERE name = "MGM"
+
+UPDATE STUDIO
+  SET president_name = "Jim Gianopulos"
+  WHERE name = "Paramount"
+
+UPDATE STUDIO
+  SET president_name = "John Johnson"
+  WHERE name LIKE "USA%"
+
+UPDATE STUDIO
+  SET president_name = "Ann Sarnoff"
+  WHERE name = "Warner Bros"
+
+SELEct president_name
+  FROM STUDIO
+  WHERE name = "MGM"
