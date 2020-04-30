b1c1f925cdf1f7240c1d7fba3ba9950f927c68ad
Niikcety <nikolaineshev1999@gmail.com>
2020-05-01 00:14:55 +0300

    Task2 added

--- Week9/Wednesday/ships.sql
+++ Week9/Wednesday/ships.sql
@@ -12,3 +12,12 @@ SELECT ships.name
           JOIN SHIPS 
           ON OUTCOMES.SHIP = SHIPS.NAME 
           WHERE DATE LIKE "1942%";    
+
+
+SELECT country, name 
+          FROM SHIPS 
+          JOIN CLASSES 
+          ON SHIPS.CLASS = CLASSES.CLASS 
+          WHERE name NOT IN ( SELECT ship 
+                              FROM OUTCOMES) 
+          ORDER BY country; 
